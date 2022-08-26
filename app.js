ae5c89ddc7752e6b25efd264e537dd8d547863c9
phillip burns <phillipworld@hotmail.co.uk>
2022-08-26 19:21:40 +0100

    Testing ejs

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
-import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import bodyParser from 'body-parser';
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import bodyParser from "body-parser";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
 const port = process.env.PORT || 8080;
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(port);
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     const now = new Date();
+    let day = now.getDay();
     const hour = now.getHours();
     const minute = now.getMinutes();
     const second = now.getSeconds();
-    res.render("list", { jsHour: hour, jsMinute: minute, jsSecond: second });
+    switch (day) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log(`There was an issue with the day: ${day}`);
+    }
+    res.render("list", { jsHour: hour, jsMinute: minute, jsSecond: second, jsDay: day });
 });
--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
-import express, { Request, Response } from 'express';
-import {fileURLToPath} from 'url';
-import {dirname} from 'path';
-import bodyParser from 'body-parser';
+import express, { Request, Response } from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import bodyParser from "body-parser";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,14 +9,42 @@ const __dirname = dirname(__filename);
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(port)
+app.listen(port);
 
-app.get('/', (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   const now: Date = new Date();
-  const hour: Number = now.getHours();
-  const minute: Number = now.getMinutes();
-  const second: Number = now.getSeconds();
-  res.render("list", { jsHour:  hour, jsMinute: minute, jsSecond: second });
-})
+  let day: any = now.getDay();
+  const hour: number = now.getHours();
+  const minute: number = now.getMinutes();
+  const second: number = now.getSeconds();
+
+  switch (day) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log(`There was an issue with the day: ${day}`);
+  }
+
+  res.render("list", { jsHour: hour, jsMinute: minute, jsSecond: second, jsDay: day});
+});
